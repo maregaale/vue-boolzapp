b676bacdc4fed68dedab044dcc2b44f438614a4c
Alessandro Marega <ale_marega@hotmail.it>
2021-03-30 14:36:20 +0200

    aggiunta risposta automatica con javascript e fix sintassi

--- js/script.js
+++ js/script.js
@@ -17,14 +17,14 @@ const app = new Vue (
               status: 'sent'
             },
             {
-                date: '10/01/2020 15:50:00',
-                message: 'Ricordati di dargli da mangiare',
-                status: 'sent'
+              date: '10/01/2020 15:50:00',
+              message: 'Ricordati di dargli da mangiare',
+              status: 'sent'
             },
             {
-                date: '10/01/2020 16:15:22',
-                message: 'Tutto fatto!',
-                status: 'received'
+              date: '10/01/2020 16:15:22',
+              message: 'Tutto fatto!',
+              status: 'received'
             }
           ],
         },
@@ -39,14 +39,14 @@ const app = new Vue (
               status: 'sent'
             },
             {
-                date: '20/03/2020 16:30:55',
-                message: 'Bene grazie! Stasera ci vediamo?',
-                status: 'received'
+              date: '20/03/2020 16:30:55',
+              message: 'Bene grazie! Stasera ci vediamo?',
+              status: 'received'
             },
             {
-                date: '20/03/2020 16:35:00',
-                message: 'Mi piacerebbe ma devo andare a fare la spesa.',
-                status: 'sent'
+              date: '20/03/2020 16:35:00',
+              message: 'Mi piacerebbe ma devo andare a fare la spesa.',
+              status: 'sent'
             }
           ],
         },
@@ -61,14 +61,14 @@ const app = new Vue (
               status: 'received'
             },
             {
-                date: '28/03/2020 10:20:10',
-                message: 'Sicuro di non aver sbagliato chat?',
-                status: 'sent'
+              date: '28/03/2020 10:20:10',
+              message: 'Sicuro di non aver sbagliato chat?',
+              status: 'sent'
             },
             {
-                date: '28/03/2020 16:15:22',
-                message: 'Ah scusa!',
-                status: 'received'
+              date: '28/03/2020 16:15:22',
+              message: 'Ah scusa!',
+              status: 'received'
             }
           ],
         },
@@ -83,9 +83,9 @@ const app = new Vue (
               status: 'sent'
             },
             {
-                date: '10/01/2020 15:50:00',
-                message: 'Si, ma preferirei andare al cinema',
-                status: 'received'
+              date: '10/01/2020 15:50:00',
+              message: 'Si, ma preferirei andare al cinema',
+              status: 'received'
             }
           ],
         },
@@ -98,34 +98,47 @@ const app = new Vue (
       },
       // FUNZIONE che aggiunge nuovo messaggio
       addNewMessage: function () {
-
         // controllo che la stringa di input sia non vuota
         if (this.newMessageText != "") {
           // aggiungo il nuovo testo del messaggio all'array dei testi dei nuovi messaggi
           this.messagesInserted.push(this.newMessageText);
-            // pusho il nuovo messaggio nella rispettiva pagina di messaggistica
-            this.contacts[this.contactsIndex].messages.push(
-              {
-                date: `${dayjs().format('DD')}/${dayjs().format('MM')}/${dayjs().format('YY')} ${dayjs().format('HH')}:${dayjs().format('mm')}:${dayjs().format('ss')}`,
-                message: this.messagesInserted[this.messagesInserted.length - 1],
-                status: 'sent'
-              },
-            );
-            // azzero il valore dell'input
-            this.newMessageText = "";
+          // pusho il nuovo messaggio nella rispettiva pagina di messaggistica
+          this.contacts[this.contactsIndex].messages.push(
+            {
+              date: `${dayjs().format('DD')}/${dayjs().format('MM')}/${dayjs().format('YY')} ${dayjs().format('HH')}:${dayjs().format('mm')}:${dayjs().format('ss')}`,
+              message: this.messagesInserted[this.messagesInserted.length - 1],
+              status: 'sent',
+            },
+          );
+          // azzero il valore dell'input
+          this.newMessageText = "";
         }
+
+        // setto la variabile che rapprersenta Vue per portarla nello scope del setTimeout
+        const myVue = this;
+
+        // risposta dopo un secondo
+        setTimeout( function () {
+          // pusho il nuovo messaggio nella rispettiva pagina di messaggistica
+          myVue.contacts[myVue.contactsIndex].messages.push(
+            {
+              date: `${dayjs().format('DD')}/${dayjs().format('MM')}/${dayjs().format('YY')} ${dayjs().format('HH')}:${dayjs().format('mm')}:${dayjs().format('ss')}`,
+              message: 'ok',
+              status: 'received',
+            },
+          );
+        }, 1000);
       },
+      // FUNZIONE che gestisce il focus sulle chat
       focus: function (index) {
         // rimuovo le classi dai list-items
         for (var i = 0; i < this.$refs.list_item.length; i++) {
-
           if (this.$refs.list_item[i].classList.contains("list_item_color") == true) {
-
             this.$refs.list_item[i].classList.remove("list_item_color");
           }
         }
         this.$refs.list_item[index].classList.add("list_item_color");
-      }
+      },
     },
   }
 );
